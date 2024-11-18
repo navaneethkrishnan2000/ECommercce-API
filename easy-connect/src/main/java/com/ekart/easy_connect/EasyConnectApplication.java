5caf63a637d474f865ce3aae36a8562163f0efdf
Navaneeth krishnan S <navaneethkrishnans2000@gmail.com>
2024-11-18 21:44:00 +0530

    initial commit

---
+++ easy-connect/src/main/java/com/ekart/easy_connect/EasyConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ekart.easy_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyConnectApplication.class, args);
+	}
+
+}
---
+++ easy-connect/src/test/java/com/ekart/easy_connect/EasyConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekart.easy_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
